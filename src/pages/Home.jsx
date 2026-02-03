5ade7a28e7daebe91ae59dc5639c1106d87fa92f
gillpann <cpundong@gmail.com>
2026-02-03 14:19:25 +0700

    initial commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import BottomNav from '../components/shared/BottomNav'
+
+const Home = () => {
+  return (
+    <section className='bg-[#1f1f1f] h-[calc(100vh-5rem)] overflow-hidden flex gap-3'>
+      {/* Left Section */}
+      <div className='flex-[3] bg-[#1a1a1a]'>
+
+      </div>
+
+      {/* Right Section */}
+      <div className='flex-[2] bg-[#1a1a1a]'>
+
+      </div>
+
+      <BottomNav />
+    </section>
+  )
+}
+
+export default Home
---
+++ src/pages/index.js
@@ -0,0 +1,7 @@
+export { default as Home } from './Home.jsx';
+export { default as Auth } from './Auth.jsx';
+export { default as Orders } from './Orders.jsx';
+
+
+
+
