7db74b678f4c026e755720bea5829411102e171d
gillpann <cpundong@gmail.com>
2026-02-05 16:09:07 +0700

    refactor(table): improve table

--- src/components/tables/TableCard.jsx
+++ src/components/tables/TableCard.jsx
@@ -1,9 +1,16 @@
-import React from 'react'
+import { useNavigate } from 'react-router-dom';
 import { getAvatarBG } from './../../utils/index';
 
-const TableCard = ({key, name, status, initials }) => {
+const TableCard = ({key, name, status, initials, seats }) => {
+
+    const navigate = useNavigate();
+    const handleClick = () => {
+        if(status === "Booked") return;
+        navigate(`/menu`)
+    }
+
   return (
-    <div key={key} className='w-[300px] bg-bg-card p-4 rounded-xl shadow-lg shadow-black/30 hover:shadow-2xl hover:-translate-y-1 transition-all duration-300 cursor-pointer'>
+    <div onClick={handleClick} key={key} className='w-[300px] bg-bg-card p-4 rounded-xl shadow-lg shadow-black/30 hover:shadow-2xl hover:-translate-y-1 transition-all duration-300 cursor-pointer'>
         <div className='flex items-center justify-between px-1'>
             <h1 className='text-text-main text-lg font-semibold'>{name}</h1>
             <p className={`${status === "Booked" ? "text-success bg-[#2e4a40]" : "text-warning  bg-[#4A3B14]"} backdrop-blur-sm px-3 py-1 rounded-full text-sm font-medium`}>
@@ -17,8 +24,8 @@ const TableCard = ({key, name, status, initials }) => {
                 >
             {initials}
             </h1>
-
         </div>
+        <p className='text-text-muted text-xs'>Seats: <span className='text-text-main'>{seats}</span></p>
     </div>
   )
 }
